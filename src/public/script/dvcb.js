9e3125cd31dd7f1b8bda39ea8c6add30a798bb80
SoulOfWindTGN <trangianghiacaibe@gmail.com>
2021-12-16 22:57:41 +0700

    fix bug dvcb
    Fix lỗi trường hợp không đặt đồ ăn.

--- src/public/script/dvcb.js
+++ src/public/script/dvcb.js
@@ -67,6 +67,7 @@ $(document).ready(function(){
             contentType: 'application/json',
             success: function(res){
                 alert("Đặt dịch vụ thành công")
+                $('body').html("<p style='text-align:center; font-size:60px'>Bạn đã đặt dịch vụ thành công, hãy quay về trang chủ ^^</p>")
                 window.location = '/'
             }
         })
