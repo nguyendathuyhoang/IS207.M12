ed26285bd7d002b047f96469130296c9d36c3b57
dangkhuong14 <f13file31@gmail.com>
2021-12-24 15:36:35 +0700

    Sua file cbctController, xoa cot MaChuyenBay

--- src/controller/cbctController.js
+++ src/controller/cbctController.js
@@ -5,12 +5,14 @@ let checkCBCT = async (req, res) => {
   // const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'datvemaybay'});
   let inputMa = req.body.maDatCho;
   let inputTen = req.body.hoTen;
-  const [duLieuKH, fields1] = await pool.execute('SELECT * FROM khachhang WHERE MaDatCho = ? And HoTen = ?', [inputMa, inputTen]);
+  const [duLieuKH] = await pool.execute('SELECT * FROM khachhang WHERE MaDatCho = ? And HoTen = ?', [inputMa, inputTen]);
   if(duLieuKH == '') res.redirect('/nhaplaiform')
 
-  let maChuyenBay = duLieuKH[0].MaChuyenBay;
-  const [duLieuCB, fields2] = await pool.execute('SELECT * FROM chuyenbay WHERE MaChuyenBay = ?', [maChuyenBay]);
-  const [duLieuDV, fields3] = await pool.execute('SELECT * FROM dichvuchuyenbay WHERE MaDatCho = ?', [inputMa]);
+  let maVe = duLieuKH[0].MaVe;
+  const [duLieuVe] = await pool.execute('SELECT * FROM vemaybay WHERE MaVe = ?', [maVe]);
+  let maChuyenBay = duLieuVe[0].MaChuyenBay;
+  const [duLieuCB] = await pool.execute('SELECT * FROM chuyenbay WHERE MaChuyenBay = ?', [maChuyenBay]);
+  const [duLieuDV] = await pool.execute('SELECT * FROM dichvuchuyenbay WHERE MaDatCho = ?', [inputMa]);
   (res.render('resultchuyenbay.ejs', { title: 'Kết quả chuyến bay của bạn', dataKH: duLieuKH, dataCB: duLieuCB, 
   dataDV: duLieuDV, maCB: maChuyenBay })); 
 }
